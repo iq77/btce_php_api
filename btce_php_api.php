805ff0d3db783efe3b2997b5e9aee3c00330b058
Marc <maerceler@hotmail.com>
2014-11-18 18:58:26 +0100

    Update btce_php_api.php

--- btce_php_api.php
+++ btce_php_api.php
@@ -5,13 +5,15 @@ class BTCE
     const
         METHOD_INFO = 'getInfo',
         METHOD_TRADE = 'Trade',
-        METHOD_LIST = 'OrderList',
+        METHOD_LIST_ACTIVE = 'ActiveOrders',
+        METHOD_CANCEL = 'CancelOrder',
         DIRECTION_BUY = 'buy',
         DIRECTION_SELL = 'sell',
         PUBLIC_API = 'https://btc-e.com/api/2/',
         PRIVATE_API = 'https://btc-e.com/tapi',
         API_KEY = 'ENTER-YOUR-BTC-E-PUBLIC-API-KEY-HERE',
-        API_SECRET = 'ENTER-YOUR-BTC-E-PRIVATE-API-KEY-HERE';
+        API_SECRET = 'ENTER-YOUR-BTC-E-PRIVATE-API-KEY-HERE',
+        USER_AGENT_NAME = 'BTC-E PHP API';
 
 /*
  * PUBLIC API
@@ -37,20 +39,38 @@ class BTCE
     }
 
     private static function getJSON($url) {
-        $context = stream_context_create(['http' => ['method' => 'GET', 'timeout' => 10]]);
-        return json_decode(file_get_contents($url, false, $context), true);
+        return json_decode(self::get_remote($url), true);
     }
 
 /*
  * PRIVATE API
  */
 
+    public static function cancelBuyOrders() {
+        $count = 0;
+        $active_orders = self::apiQuery(self::METHOD_LIST_ACTIVE);
+        if ($active_orders) {
+            $success = $active_orders->{'success'};
+            if ($success == YES) {
+                $orderlist = get_object_vars($active_orders->{'return'});
+                foreach($orderlist as $varname=>$value) {
+                    $obj = $value;
+                    if ($obj->{'type'}=='buy') {
+                        self::apiQuery(self::METHOD_CANCEL, ['order_id'=>$varname]);
+                        $count++;
+                    }
+                }
+            }
+        }
+        return $count;
+    }
+
     public static function placeOrder($amount, $pair, $direction, $price) {
         $result = self::apiQuery(self::METHOD_TRADE , ['pair'=>$pair, 'type'=>$direction, 'rate'=>$price, 'amount'=>$amount]);
         $out = false;
         if ($result) {
             $success = $result->{'success'};
-            if ($success == 1)
+            if ($success == YES)
                 $out = true;
         }
         return $out;
@@ -60,37 +80,43 @@ class BTCE
         return self::apiQuery(self::METHOD_INFO);
     }
 
-    private static function apiQuery($method, $req = []) {
+
+/*
+ * subroutines
+ */
+
+    private function apiQuery($method, $req = []) {
         $req['method'] = $method;
         $req['nonce'] = self::nonce();
-         // Generate the keyed hash value to post
         $sign = hash_hmac("sha512", http_build_query($req, '', '&'), self::API_SECRET);
-         // Add to the headers
         $key = self::API_KEY;
         $headers = ["Sign: {$sign}", "Key: {$key}"];
-        // Create a CURL Handler for use
-        $res = self::do_post_request(self::PRIVATE_API, $req, $headers);
-        $out = json_decode($res);
+        $result = self::get_remote(self::PRIVATE_API, $req, $headers);
+        $out = json_decode($result);
         if(!$out || !isset($out->{'success'}) || $out->{'success'}!=1)
             $out = false;
         return $out;
     }
 
-    private static function do_post_request($url, $postfields, $headers) {
+    private static function get_remote($url, $postfields=null, $headers=null) {
         $content = '';
         if (in_array('curl', get_loaded_extensions())) {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_TIMEOUT        , REMOTE_TIMEOUT);
             curl_setopt($ch, CURLOPT_URL            , $url);
-            curl_setopt($ch, CURLOPT_HEADER         , 0);
+            curl_setopt($ch, CURLOPT_HEADER         , false);
             curl_setopt($ch, CURLOPT_FAILONERROR    , true);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER , true);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION , true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER     , $headers);
-            curl_setopt($ch, CURLOPT_USERAGENT      , 'Mozilla/4.0 (compatible; BTC-E PHP API; '.php_uname('s').'; PHP/'.phpversion().')');
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($ch, CURLOPT_POST           , count($postfields));
-            curl_setopt($ch, CURLOPT_POSTFIELDS     , http_build_query($postfields, '', '&'));
+            if (!is_null($headers)) {
+                curl_setopt($ch, CURLOPT_HTTPHEADER , $headers);
+            }
+            curl_setopt($ch, CURLOPT_USERAGENT      , 'Mozilla/4.0 (compatible; '.self::USER_AGENT_NAME.'; '.php_uname('s').'; PHP/'.phpversion().')');
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , false);
+            if (is_array($postfields)) {
+                curl_setopt($ch, CURLOPT_POST       , count($postfields));
+                curl_setopt($ch, CURLOPT_POSTFIELDS , http_build_query($postfields, '', '&'));
+            }
             $content = curl_exec($ch);
             if (curl_errno($ch)>0)
                 $content = false;
@@ -98,6 +124,7 @@ class BTCE
         }
         return $content;
     }
+
 
     private static function nonce() {
     /*
